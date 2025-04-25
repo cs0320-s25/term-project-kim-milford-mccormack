ef403ac9f71a2167d1481911e09ce75a5d666c84
hadley-mccormack <hadley_mccormack@brown.edu>
2025-04-25 17:11:08 -0400

    feat: places api working, parameters not dynamic

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -1,3 +1,20 @@
-public class Server {
+package server;
 
+import com.sun.net.httpserver.HttpServer;
+import handlers.PlacesHandler;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Server {
+  public static void main(String[] args) throws IOException {
+    int port = 8080;
+    HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+    server.createContext("/places", new PlacesHandler());
+
+    server.setExecutor(null);
+    server.start();
+    System.out.println("Server started on http://localhost:" + port + "/places");
+  }
 }
--- server/src/main/java/handlers/GooglePlacesClient.java
+++ server/src/main/java/handlers/GooglePlacesClient.java
@@ -1,40 +1,34 @@
 package handlers;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import org.json.JSONArray;
-import org.json.JSONObject;
+
 
 public class GooglePlacesClient {
 
-  private static final String API_KEY = System.getenv("GOOGLE_API_KEY");
+  private static final String API_KEY = "AIzaSyD9CVq2ghzKOixMiMF4qjbksTnlwYHaGjM";
 
-  public static void search(String keyword, double lat, double lng) throws Exception {
-    String encodedKeyword = keyword.replace(" ", "%20");
-    String endpoint = String.format(
-        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%f,%f&radius=2000&keyword=%s&key=%s",
-        lat, lng, encodedKeyword, API_KEY
+  private final HttpClient client;
+
+  public GooglePlacesClient() {
+    this.client = HttpClient.newHttpClient();
+  }
+
+  public String searchNearbyAsJson(double lat, double lng, int radius, String keyword) throws IOException, InterruptedException {
+    String uri = String.format(
+        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%f,%f&radius=%d&keyword=%s&key=%s",
+        lat, lng, radius, keyword, API_KEY
     );
 
-    HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create(endpoint))
+        .uri(URI.create(uri))
         .GET()
         .build();
 
     HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-    JSONObject json = new JSONObject(response.body());
-
-    JSONArray results = json.getJSONArray("results");
-    for (int i = 0; i < results.length(); i++) {
-      JSONObject place = results.getJSONObject(i);
-      System.out.println(place.getString("name"));
-    }
-  }
-
-  public static void main(String[] args) throws Exception {
-    search("cozy cafe", 37.7749, -122.4194);
+    return response.body();
   }
 }
