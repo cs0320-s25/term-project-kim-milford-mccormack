374539855544f5c77d289699bba7f1542703572a
hadley-mccormack <hadley_mccormack@brown.edu>
2025-05-05 11:49:01 -0400

    resolving merge conflict

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"EGxm1G71CZ0eg2noeu2D1Gml6+I19CVtweXXX2ir72U=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UIIJKro0qwjbeoZYOaP5RNEgN8Zl5tsdohlrQfSYE2o=\"\n}"
