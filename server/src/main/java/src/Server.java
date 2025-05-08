1488f5cede5f28da45015b9d46d52b375932243c
daniellekim <flydanielle09@gmail.com>
2025-05-07 23:44:20 -0400

    created mock data handler

--- server/src/main/java/src/Server.java
+++ server/src/main/java/src/Server.java
@@ -3,6 +3,7 @@ package src;
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import src.handlers.MockPlacesHandler;
 import src.handlers.PlacesHandler;
 
 public class Server {
@@ -10,10 +11,16 @@ public class Server {
     int port = 8080;
     HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
-    server.createContext("/places", new PlacesHandler());
+//    server.createContext("/places", new PlacesHandler());
+
+    //mock data handler for below sample URL
+    // http://localhost:8080/places?lat=41.8286671&lng=-71.4086326&radius=1000&keyword=cafe
+    server.createContext("/places", new MockPlacesHandler());
 
     server.setExecutor(null);
     server.start();
     System.out.println("Server started on http://localhost:" + port + "/places");
+
+
   }
 }
